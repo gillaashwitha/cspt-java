58dc7323a3f06ed447ccf5044292c0e9046e36d2
gillaashwitha <gillaashwitha@gmail.com>
2026-07-31 08:20:50 +0000

    added

--- CSPT1-170726/CSPT2-180726/AbsDemo.java
+++ CSPT1-170726/CSPT2-180726/AbsDemo.java
@@ -1,20 +1,18 @@
 abstract class DemoAB{
+    
     abstract public void show();
     public void display(){
-        IO.println("Display Method");
+        System.out.println("Display Method");
     }
 }
-class AbsDemo extends DemoAB
-{
+class AbsDemo extends DemoAB{
     @Override
-    public void show()
-    {
-        IO.println("I am SHOW FROM MEESHO");
+    public void show(){
+        System.out.println("I am Show from meesho");
     }
-
-    public static void main(String args[])
-    {
+    public static void main(String args[]){
         AbsDemo dab = new AbsDemo();
         dab.display();
     }
-}   
+}
+        
